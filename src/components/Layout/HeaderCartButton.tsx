a8a466127915451e5dc79d6e91ef4c21ea53c6fb
George Henrique Wurthmann <gemakai@gmail.com>
2022-04-17 16:36:10 -0300

    Add missing file to commit

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import { CartButton, CartBadge } from './HeaderCartButton.styles';
 import CartContext from '../../store/cart-context';
@@ -8,14 +8,32 @@ interface HeaderCartButtonProps {
 }
 
 const HeaderCartButton = (props: HeaderCartButtonProps) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+  
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
-  }, 0)
+  }, 0);
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
-    <CartButton onClick={props.onClick}>
+    <CartButton isAnimated={btnIsHighlighted} onClick={props.onClick}>
       <span>
         <CartIcon />
       </span>
